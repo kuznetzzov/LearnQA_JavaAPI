346d4b758e34f9bdb9766cff4ee776c68336f1d4
kuznetzzov <kuznetsoff.ma@gmail.com>
2023-03-25 00:13:26 +0300

    FirstGetReq

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,10 +1,15 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
 public class HelloWorldTest {
 
     @Test
     public void helloWorldTest(){
-        System.out.println("Hello from Maksim Kuznetsov!");
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/get_text")
+                .andReturn();
+        response.prettyPrint();
     }
 
 }
